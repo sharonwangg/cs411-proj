61525ccc1a87ec0f04c4f8f40b306e4683d3d154
Sarthak67 <mishra.sarthak67@gmail.com>
2020-11-01 14:21:31 -0600

    Update basic_insert.py
    added delete, update, search , login
    
    Co-Authored-By: rchellani <72985430+rchellani@users.noreply.github.com>

--- cs411_project/back/basic_insert.py
+++ cs411_project/back/basic_insert.py
@@ -4,26 +4,146 @@ import urllib.request as urllib
 import zipfile
 import pymysql.cursors
 import json
-
+import hashlib 
 import flask
 
+
+def login(username,password):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+	try: 
+		with connection.cursor() as cur:
+			cur.execute('Select username,email,age from login where username = %s and password = %s', ( username, password)) 
+			rows = cur.fetchall()
+			if(len(rows)==0):
+				print('wrong Username or password')
+			else:
+				for i in rows:
+					print('Username :'+i[0])
+					print('email :'+i[1])
+					print('age :'+str(i[2]))
+
+
+	finally:
+	    connection.close()
+
+
+
+def create_user(username,password,email,age):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+
+	try:
+	    with connection.cursor() as cursor:
+	        sql = "INSERT INTO `user` (`username`) VALUES (%s)"
+	        cursor.execute(sql, (username))
+	        #password_hash= hashlib.md5(password).hexdigest() 
+	        sql = "INSERT INTO `login` (`username`,`password`,`email`,`age`) VALUES (%s,%s,%s,%s)"
+	        cursor.execute(sql, (username,password,email,age))
+	    connection.commit()
+	finally:
+	    connection.close()
+
+
+
+
+
+def create_post(username,text,date,book_id):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+
+	try:
+	    with connection.cursor() as cursor:
+	        sql = "INSERT INTO `post` (`username`,`text1`, `dateTime`,`book_id`) VALUES (%s, %s, %s, %s)"
+	        cursor.execute(sql, (username,text,date,book_id))
+	    connection.commit()
+	finally:
+	    connection.close()
+
+
+def delete(value):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+
+	try:
+	    with connection.cursor() as cursor:
+	        sql = "delete from `post` where post_id=(%s)"
+	        cursor.execute(sql, (value))
+	    connection.commit()
+	finally:
+	    connection.close()
+
+
+	
+
+def search(value):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+
+
+
+	try: 
+
+	    with connection.cursor() as cur:
+
+	            
+	        cur.execute('Select username, text1,dateTime from post where text1 like %s or username like %s', ("%" + value + "%","%" + value + "%")) 
+	        rows = cur.fetchall()
+	        for i in rows:
+		        print('Username :'+i[0])
+		        print('text :'+i[1])
+		        print('dateTime :'+str(i[2]))
+
+
+
+	finally:
+	    connection.close()
+
+
+
+
+def edit(post_id,value):
+	connection = pymysql.connect(host='localhost',
+	                             user='root',
+	                             password='admin',
+	                             db='book_club')
+
+	try:
+	    with connection.cursor() as cursor:
+	        sql = "update `post` set text1=%s where post_id=%s"
+	        cursor.execute(sql, (value,str(post_id)))
+	    connection.commit()
+	finally:
+	    connection.close()
+
+
+
+
+
+
 app = flask.Flask("__main__")
-
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='cs411',
-                             db='book_club')
-
-try:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO `post` (`username`,`text1`, `dateTime`,`book_id`) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, ('robin','dgrsdgdsf','2019-01-01','12'))
-    connection.commit()
-finally:
-    connection.close()
-
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token = "hello")
+	#create_user('superman','super123','superman@gmail.com','20')
+	login('superman','saf')
+	#print('--user Created---')
+	#create_post('batman','adgs','2020-01-01','12')
+	#print('---inserted---')
+	#delete(5)
+	#print('---delted=---')
+	search('batman')
+	print('---search result above----')
+	#edit(6,'batman')
+	return flask.render_template("index.html", token = "hello")
 
 app.run(debug = True)
